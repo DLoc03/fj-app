b5ad541a7db7818311c775be06cf291715de392c
Nghia Le Tuan <nghia.le@titancorpvn.com>
2025-03-10 21:09:34 +0700

    Update login and refresh token

--- server/src/service/auth.service.js
+++ server/src/service/auth.service.js
@@ -7,22 +7,29 @@ const SALT = 10
 const login = async (email, password) => {
     try {
         const user = await User.findOne({ email });
-        if (!user) return { errCode: 1, message: "No user found" };
+        if (!user) return { errCode: 1, message: "User not found" };
 
-        const isMatchPassword = bcrypt.compareSync(password, user.password);
+        const isMatchPassword = bcrypt.compare(password, user.password);
         if (!isMatchPassword) return { errCode: 2, message: "Incorrect password" };
-
 
         const accessToken = generateToken(user);
         const refreshToken = generateRefreshToken(user);
-
-
         user.refreshToken = refreshToken;
         await user.save();
-
-        return { errCode: 0, message: "Login successful", accessToken, refreshToken };
+        return {
+            accessToken,
+            refreshToken,
+            user: {
+                id: user._id,
+                email: user.email,
+                name: user.name,
+                phone: user.phone,
+                role: user.role
+            }
+        };
     } catch (error) {
         console.error(error);
+        throw new Error("Internal Server Error")
     }
 }
 
@@ -64,15 +71,31 @@ const logout = async (id) => {
 }
 
 const refreshAccessToken = async (refreshToken) => {
-    const user = await User.findOne({ refreshToken })
-    if (!user) return { errCode: 1, message: "Invalid refresh token" }
+    if (!refreshToken) {
+        return {
+            errCode: 1,
+            message: 'No refresh token provided'
+        }
+    }
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET)
 
-    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
-        if (err) return { errCode: 2, message: "Token expired" }
-    })
+    if (!decoded || !decoded.id) {
+        return { errCode: 2, message: "Invalid refresh token" }
+    }
 
-    const newAccessToken = generateToken(user)
-    return { errCode: 0, accessToken: newAccessToken, refreshToken: refreshToken };
+    const user = await User.findById({ _id: decoded.id })
+    if (!user || user.refreshToken !== refreshToken) {
+        return {
+            errCode: 3,
+            message: "Invalid refresh token"
+        }
+    }
+    const newAccessToken = generateToken(user);
+    return {
+        errCode: 0,
+        message: 'Re-new access-token succeed',
+        accessToken: newAccessToken
+    }
 }
 
 const generateToken = (user) => {
