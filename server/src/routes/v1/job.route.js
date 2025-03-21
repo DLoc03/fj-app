64769fb672b719345a59a309fd633be311861512
Nghia Le Tuan <nghia.le@titancorpvn.com>
2025-03-21 15:10:44 +0700

    Full feature for job

--- server/src/routes/v1/job.route.js
+++ server/src/routes/v1/job.route.js
@@ -6,5 +6,10 @@ const Router = express.Router()
 
 Router.route('/')
     .post(verifyToken, authorizeAdmin('user'), jobController.postJob)
-
+Router.route('/:id')
+    .get(jobController.getJob)
+Router.route('/:id')
+    .put(verifyToken, authorizeAdmin('user'), jobController.updateJobById)
+Router.route('/')
+    .get(jobController.getJobs)
 export const jobRoute = Router
