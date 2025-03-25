0db68c85516ca7b3cf06376026fb53840ee988be
Nghia Le Tuan <nghia.le@titancorpvn.com>
2025-03-25 23:20:05 +0700

    Answer question feature and create question for 1 job

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -3,10 +3,16 @@ import { authRoutes } from "./auth.route.js";
 import { userRoute } from "./user.route.js";
 import { companyRoute } from "./company.route.js";
 import { jobRoute } from "./job.route.js";
+import { questionRoute } from "./question.route.js";
+import { applicantRoute } from "./applicant.route.js";
+import { answerRoute } from "./answer.route.js";
 const Router = express.Router()
 
 Router.use('/auth', authRoutes)
 Router.use('/user', userRoute)
 Router.use('/comp', companyRoute)
 Router.use('/job', jobRoute)
+Router.use('/question', questionRoute)
+Router.use('/applicant', applicantRoute)
+Router.use('/answer', answerRoute)
 export const API_ROUTE = Router
