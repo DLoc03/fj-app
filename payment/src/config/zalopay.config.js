efcdb8d27b688aa5b0bd6bcf97b994d78a6851de
Nghia Le Tuan <nghia.le@titancorpvn.com>
2025-04-21 13:57:42 +0700

    Delete payment server

--- payment/src/config/zalopay.config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    app_id: "2554",
-    key1: "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn",
-    key2: "trMrHtvjo6myautxDUiAcYsVtaeQ8nhf",
-    CREATE: "https://sb-openapi.zalopay.vn/v2/create",
-    QUERY: "https://sb-openapi.zalopay.vn/v2/query",
-    REFUND: "https://sb-openapi.zalopay.vn/v2/refund",
-    STATUS: "https://sandbox.zalopay.com.vn/v001/tpe/getpartialrefundstatus"
-}
--- payment/src/index.js
+++
@@ -1,9 +0,0 @@
-import { app } from "./app.js";
-import { createServer } from 'http';
-import { socketServer } from "./socket/index.js";
-
-const httpServer = createServer(app);
-socketServer(httpServer)
-httpServer.listen(3030, () => {
-    console.log(`Payment server is running on: http://localhost:3030`);
-})
