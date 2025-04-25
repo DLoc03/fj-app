045ccee25b544d29d70ea58802d70024005b16e4
Nghia Le Tuan <nghia.le@titancorpvn.com>
2025-04-25 10:04:08 +0700

    Format worker and log server

--- audit_logs_server/src/utils/master.response.js
+++ audit_logs_server/src/utils/master.response.js
@@ -1,13 +1,18 @@
-import { ERROR_CODE, STATUS } from "../utils/enum.js"
+import { ERROR_CODE, STATUS } from "../utils/enum.js";
 
 export const MasterResponse = (result) => {
-    const { status = STATUS.DONE, errCode = ERROR_CODE.DONE, message = 'OK', data = null } = result
-    return {
-        status,
-        result: {
-            errCode,
-            message,
-            data
-        }
-    }
-}
+  const {
+    status = STATUS.DONE,
+    errCode = ERROR_CODE.DONE,
+    message = "OK",
+    data = null,
+  } = result;
+  return {
+    status,
+    result: {
+      errCode,
+      message,
+      data,
+    },
+  };
+};
--- worker/configs/db.config.js
+++ worker/configs/db.config.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
+import "dotenv/config";
 async function CONNECT_DB() {
-    try {
-        await mongoose.connect(process.env.DB_URI)
-        console.log('Mongodb connected successfully')
-    } catch (error) {
-        console.error('Mongodb conect failed', error);
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Mongodb connected successfully");
+  } catch (error) {
+    console.error("Mongodb conect failed", error);
+    process.exit(1);
+  }
 }
-export { CONNECT_DB }
+export { CONNECT_DB };
