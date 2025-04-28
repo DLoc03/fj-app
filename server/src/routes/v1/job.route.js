1b283d91ef67a5fa0e1ef098deb47ef3e20e328e
Nghia Le Tuan <nghia.le@titancorpvn.com>
2025-04-28 11:55:19 +0700

    Recovery cache

--- server/src/routes/v1/job.route.js
+++ server/src/routes/v1/job.route.js
@@ -9,10 +9,7 @@ import cacheMiddleware from "../../middleware/cache.middleware.js";
 const Router = express.Router();
 
 Router.route("/")
-  .get(
-    // cacheMiddleware,
-    jobController.getJobs
-  )
+  .get(cacheMiddleware, jobController.getJobs)
 
   .post(
     checkBlackList,
